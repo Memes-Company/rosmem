e45abf91ff38d10b17f59ebee3411937a0938956
Vaagn Avanesyan <vahagn1993@gmail.com>
2019-10-13 11:05:36 +0300

    Get token and create forks

--- prototypes/web-commit-prototype/src/App.tsx
+++ prototypes/web-commit-prototype/src/App.tsx
@@ -4,17 +4,19 @@ import React from 'react';
 import { GithubService } from './github-service';
 
 const App: React.FC = () => {
-  const authLink = "https://github.com/login/oauth/authorize?client_id=ba97c6160162014cccdc&scope=user%20repo";
+  const authLink = GithubService.getAuthLink();
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get('code');
-  //TODO: need to save it into Context or any another place but NOT into the localstorage
+  if (code) {
+    GithubService.setToken(code);
+  }
   return (
     <div className="App">
       <header className="App-header">
         {code ? (
           <>
             <p> Yor Github code is {code}</p>
-            <button onClick={() => GithubService.createFork(code, 'kosachemoto', 'test-readme-repo')}>Create a fork</button>
+            <button onClick={() => GithubService.createFork('kosachemoto', 'test-readme-repo')}>Create a fork</button>
           </>
         ) :
           (
--- prototypes/web-commit-prototype/src/github-service.ts
+++ prototypes/web-commit-prototype/src/github-service.ts
@@ -1,27 +1,31 @@
 export class GithubService {
-  private static endpoint = 'https://api.github.com';
-  public static async createFork(code: string, sourceRepoOwner: string, sourceRepoName: string) {
-    const data = JSON.stringify({
-      client_id: 'ba97c6160162014cccdc',
-      client_secret: 'b21353e4e21996d91575598c76957edd00e72696',
-      code: code,
-    });
-    const token = await fetch(`https://github.com/login/oauth/access_token`, {
+  private static apiEndpoint = 'https://api.github.com';
+  private static clientId = 'ba97c6160162014cccdc';
+  private static clientSecret = 'b21353e4e21996d91575598c76957edd00e72696';
+  static token?: string;
+  public static getAuthLink() {
+    const scopes = ['user', 'repo'].join('%20');
+    return `https://github.com/login/oauth/authorize?client_id=${this.clientId}&scope=${scopes}`;
+  }
+  public static async setToken(code: string) {
+    const form = new FormData();
+    form.append('client_id', this.clientId);
+    form.append('client_secret', this.clientSecret);
+    form.append('code', code);
+    this.token = await fetch('https://github.com/login/oauth/access_token', {
       method: 'post',
-      mode: 'no-cors',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: data,
-    });
-    return fetch(`${this.endpoint}/${sourceRepoOwner}/${sourceRepoName}/forks`, {
+      body: form,
+    })
+      .then((response) => response.text())
+      .then((s) => new URLSearchParams(s).get('access_token') || undefined);
+  }
+  public static async createFork(sourceRepoOwner: string, sourceRepoName: string) {
+    this.token = this.token;
+    fetch(`${this.apiEndpoint}/repos/${sourceRepoOwner}/${sourceRepoName}/forks`, {
       method: 'post',
-      mode: 'cors',
       headers: {
-        Accept: 'application/json',
         'Content-Type': 'application/json',
-        Authorization: `token ${token}`,
+        Authorization: `token ${this.token}`,
       },
     });
   }
