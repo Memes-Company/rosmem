64d44360bf1537a48824daa57a09509595b4ca5a
Vaagn Avanesyan <vahagn1993@gmail.com>
2019-10-13 11:08:40 +0300

    Fix code issues

--- prototypes/web-commit-prototype/src/github-service.ts
+++ prototypes/web-commit-prototype/src/github-service.ts
@@ -1,18 +1,19 @@
 export class GithubService {
   private static apiEndpoint = 'https://api.github.com';
+  private static ghEndpoint = 'https://github.com';
   private static clientId = 'ba97c6160162014cccdc';
   private static clientSecret = 'b21353e4e21996d91575598c76957edd00e72696';
   static token?: string;
   public static getAuthLink() {
     const scopes = ['user', 'repo'].join('%20');
-    return `https://github.com/login/oauth/authorize?client_id=${this.clientId}&scope=${scopes}`;
+    return `${this.ghEndpoint}/login/oauth/authorize?client_id=${this.clientId}&scope=${scopes}`;
   }
   public static async setToken(code: string) {
     const form = new FormData();
     form.append('client_id', this.clientId);
     form.append('client_secret', this.clientSecret);
     form.append('code', code);
-    this.token = await fetch('https://github.com/login/oauth/access_token', {
+    this.token = await fetch(`${this.ghEndpoint}/login/oauth/access_token`, {
       method: 'post',
       body: form,
     })
