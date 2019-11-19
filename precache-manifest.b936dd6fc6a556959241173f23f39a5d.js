a6197bd0a7cf4415055f0dcc54826ddead1bb11a
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-11-19 03:54:03 +0300

    Updates

---
+++ precache-manifest.b936dd6fc6a556959241173f23f39a5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57c9671d485e7aff12148e9d39825d7",
+    "url": "/rosmem/index.html"
+  },
+  {
+    "revision": "9c2daf0048772d68f685",
+    "url": "/rosmem/static/css/main.4ea2e97b.chunk.css"
+  },
+  {
+    "revision": "1927d9570dd34315d1ca",
+    "url": "/rosmem/static/js/2.2b93c3e3.chunk.js"
+  },
+  {
+    "revision": "9c2daf0048772d68f685",
+    "url": "/rosmem/static/js/main.d3adabf4.chunk.js"
+  },
+  {
+    "revision": "71b744772cdfc7a1ac5e",
+    "url": "/rosmem/static/js/runtime-main.7fa59d4a.js"
+  },
+  {
+    "revision": "63aa816be4bb934e744ae217fb47d9bb",
+    "url": "/rosmem/static/media/logoname.63aa816b.svg"
+  }
+]);
