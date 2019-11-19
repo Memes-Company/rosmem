b0e65f6761ed6fc233e882441fd0e04e9c234181
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-11-19 04:01:05 +0300

    Updates

---
+++ precache-manifest.f333218dcce99b2afd24e2fc1e7b69bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8cd8ff27088c6273162b23b2d8783eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5cd16b56f650078cd22",
+    "url": "/static/css/main.4ea2e97b.chunk.css"
+  },
+  {
+    "revision": "1927d9570dd34315d1ca",
+    "url": "/static/js/2.2b93c3e3.chunk.js"
+  },
+  {
+    "revision": "a5cd16b56f650078cd22",
+    "url": "/static/js/main.6adaf8fa.chunk.js"
+  },
+  {
+    "revision": "81b8050506ffe15004bd",
+    "url": "/static/js/runtime-main.4bd6223e.js"
+  },
+  {
+    "revision": "63aa816be4bb934e744ae217fb47d9bb",
+    "url": "/static/media/logoname.63aa816b.svg"
+  }
+]);
