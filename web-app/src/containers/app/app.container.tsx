26b801fdc644ae8f5f05930f0cd81fdc946b0a51
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-11-23 10:25:34 +0300

    Fix incorrect representation of the grid in mobile browsers

--- web-app/src/containers/app/app.container.tsx
+++ web-app/src/containers/app/app.container.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { 
   Header, 
   Body, 
   Footer 
 } from '../../containers';
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 import './app.container.css';
 
 export const App = () => {
+  useEffect(() => {
+    fromEvent(window, 'resize').pipe(
+      map((event: Event): Window => {
+        return event.target as Window;
+      })
+    ).subscribe((window: Window): void => {
+      const viewportVH = window.innerHeight * 0.01;
+      window.document.documentElement.style.setProperty('--viewportVH', `${viewportVH}px`);
+    });
+
+    window.dispatchEvent(new Event('resize'));
+  }, []);
+
   return (
     <div className="app">
       <Header class="app__header" />
