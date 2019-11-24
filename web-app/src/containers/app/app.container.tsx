ecc4a0fff9453c314bb56ea421a5948818b12ff4
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-11-24 03:53:34 +0300

    Fix incorrect css calculations on the production build

--- web-app/src/containers/app/app.container.tsx
+++ web-app/src/containers/app/app.container.tsx
@@ -5,18 +5,27 @@ import {
   Footer 
 } from '../../containers';
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import './app.container.css';
 
 export const App = () => {
   useEffect(() => {
-    fromEvent(window, 'resize').pipe(
-      map((event: Event): Window => {
-        return event.target as Window;
+    fromEvent<React.ChangeEvent<Window>>(window, 'resize').pipe(
+      tap((event: React.ChangeEvent<Window>): void => {
+        const viewportVH = event.target.innerHeight * 0.01;
+        event.target.document.documentElement.style.setProperty('--viewportVH', `${viewportVH}px`);
+      }),
+      map((event: React.ChangeEvent<Window>): number => {
+          return event.target.pageYOffset;
       })
-    ).subscribe((window: Window): void => {
-      const viewportVH = window.innerHeight * 0.01;
-      window.document.documentElement.style.setProperty('--viewportVH', `${viewportVH}px`);
+    ).subscribe((pageYOffset: number): void => {
+      if (pageYOffset < 0) {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
     });
 
     window.dispatchEvent(new Event('resize'));
