8348e1900df06482cb2c3aa761695fb7b75d2b37
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-11-24 10:41:11 +0300

    Add complex improvements of the mobile appearance (#4)
    * Remove unnecessary code + fix all warnings
    
    * Fix footer background color changing mechanism
    
    * Fix incorrect representation of the grid in mobile browsers
    
    * Remove footer-title component
    
    * Fix incorrect css calculations on the production build

--- web-app/src/containers/footer/footer.container.tsx
+++ web-app/src/containers/footer/footer.container.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { BaseProps } from '../../types';
-import { fromEvent, pipe, of, from } from 'rxjs';
-import { map, tap, filter } from 'rxjs/operators';
+import { fromEvent, BehaviorSubject } from 'rxjs';
+import { map, tap, filter, distinctUntilChanged } from 'rxjs/operators';
 import {ReactComponent as Logoname} from '../../assets/svg/logoname.svg';
 import './footer.container.css';
 
@@ -12,32 +12,48 @@ export const Footer = (props: BaseProps) => {
   const footerBodyElement = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const matchRGB: RegExp = new RegExp(/.+\((\d+)\,\s(\d+)\,\s(\d+).+/);
-    const [, red, green, blue] = footerElement.current!.style.backgroundColor!.match(matchRGB)!;
+    const getRGBAstring = (red: number = 0, green: number = 0, blue: number = 0, opacity: number = 0): string => {
+      return `rgba(${red}, ${green}, ${blue}, ${opacity})`;
+    }
+
+    const isFooterBackgroundChanging = new BehaviorSubject(false);
+    const matchRGB: RegExp = new RegExp(/.+\((\d+),\s(\d+),\s(\d+).+/);
+    const [, red, green, blue] = footerElement.current!.style.backgroundColor!.match(matchRGB)! as [unknown, number, number, number];
+
+    isFooterBackgroundChanging.pipe(
+        distinctUntilChanged(),
+        // Trigger only when it's not changing
+        filter(value => !value)
+      ).subscribe(value => {
+        footerElement.current!.style.backgroundColor = getRGBAstring(red, green, blue);
+      });
 
     const subscription = fromEvent(document, 'scroll').pipe(
         // Number of pixels of footer content element showed on the screen
-        map((value: any) => {
+        map((value: any): number => {
           return window.innerHeight - footerBodyElement.current!.getBoundingClientRect().top;
+        }),
+        tap((scrollHeight : number): void => {
+          isFooterBackgroundChanging.next(scrollHeight >= 0);
         }),
         // Trigger only when footer content element on the screen
         filter((scrollHeight: number) => {
           return scrollHeight >= 0;
         }),
         // Convert to percentages of footer showing 
-        map((scrollHeight: number) => {
+        map((scrollHeight: number): number => {
           return scrollHeight / footerBodyElement.current!.clientHeight;
         }),
-        tap((scrollPercentages: number) => {
+        tap((scrollPercentages: number): void => {
           setIsTestLight(scrollPercentages >= .8);
         })
-      ).subscribe((scrollPercentages: number) => {
-        footerElement.current!.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${scrollPercentages})`;
+      ).subscribe((scrollPercentages: number): void => {
+        footerElement.current!.style.backgroundColor = getRGBAstring(red, green, blue, scrollPercentages);
       });
     return () => {
       subscription.unsubscribe();
     }
-  });
+  }, []);
   return (
     <div
       ref={footerElement}
--- web-app/src/containers/header/header.container.tsx
+++ web-app/src/containers/header/header.container.tsx
@@ -5,7 +5,7 @@ import './header.container.css';
 export const Header = (props: BaseProps) => {
   return (
     <div className={"header" + (props.class ? " " + props.class : "")}>
-      <span className="header-title">Header</span>
+      <input type="text" placeholder="Header?" />
     </div>
   )
 }
