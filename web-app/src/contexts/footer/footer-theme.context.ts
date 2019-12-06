d9e447c0f6ff43144934e5967de6653dcbb82924
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-12-07 01:57:08 +0300

    Add theme context for footer element

---
+++ web-app/src/contexts/footer/footer-theme.context.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { FooterTheme } from '../../types';
+
+export const FooterThemeContext = React.createContext<FooterTheme>("light");
--- web-app/src/types/index.ts
+++ web-app/src/types/index.ts
@@ -1,2 +1,3 @@
 export * from './shared';
 export * from './modal';
+export * from './footer';
