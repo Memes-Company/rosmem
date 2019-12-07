d2d79d522bf4f611ce960e71fa23bed5bf61a937
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-12-08 01:50:50 +0300

    Add Footer Section component

--- web-app/src/components/index.ts
+++ web-app/src/components/index.ts
@@ -2,3 +2,4 @@ export * from './modal';
 export * from './link';
 export * from './footer-link';
 export * from './footer-title';
+export * from './footer-section';
--- web-app/src/types/index.ts
+++ web-app/src/types/index.ts
@@ -2,4 +2,5 @@ export * from './shared';
 export * from './modal';
 export * from './footer';
 export * from './footer-link';
+export * from './footer-section';
 export * from './link';
