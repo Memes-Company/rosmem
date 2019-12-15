719702c10c7a8009ec9a19c604a1157bef495961
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2019-12-15 18:42:23 +0300

    Add Button component

--- web-app/src/components/index.ts
+++ web-app/src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './modal';
 export * from './link';
+export * from './button';
 export * from './footer-link';
 export * from './footer-title';
 export * from './footer-section';
--- web-app/src/types/index.ts
+++ web-app/src/types/index.ts
@@ -4,4 +4,5 @@ export * from './footer';
 export * from './footer-link';
 export * from './footer-section';
 export * from './link';
+export * from './button';
 export * from './l10n';
