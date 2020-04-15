2dbd27f1f3d015e9bfb36b3c017a1710693b7bfb
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2020-04-16 02:40:05 +0300

    feature/complex-refactoring: Incapsulating part of the footer viewport functionality in render props module

---
+++ web-app/src/modules/scroll-tracker/viewport-tracker.module.types.ts
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Subscription } from 'rxjs';
+
+export interface Props {
+  children: (targetProps: TargetProps) => React.ReactNode;
+}
+
+export interface State extends TargetProps {
+  subscription?: Subscription;
+}
+
+export interface TargetProps {
+  forwardedRef: React.RefObject<HTMLElement>;
+  progress: number;
+}
---
+++ web-app/src/modules/viewport-tracker/index.ts
@@ -0,0 +1,4 @@
+import { ViewportTracker } from './viewport-tracker.module';
+import * as ViewportTrackerTypes from './viewport-tracker.module.types';
+
+export { ViewportTracker, ViewportTrackerTypes };
